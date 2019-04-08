43e877b01b76b6aab39a1d1998f9c2674fd8bc2b
2fedjs <49389822+2fedjs@users.noreply.github.com>
2019-04-08 12:30:56 +0500

    wddwq
    wqddwq

---
+++ script.js
@@ -0,0 +1,21 @@
+﻿ window.addEventListener('load',function() {
+var canvas = document.querySelector('#playground');
+    console.log(canvas.innerHTML);
+var ScreenSize ={
+    WIDTH:800,
+    HEIGHT:600
+   };
+
+   var setup = function () {
+    var DROPS = 600;
+    var canvas = document.querySelector('#playground');
+    console.log(canvas.innerHTML);
+    var ctx = canvas.getContext('2d');
+
+    canvas.width = ScreenSize.WIDTH;
+    canvas.height = ScreenSize.HEIGHT;
+   }
+
+   setup();
+
+   })
